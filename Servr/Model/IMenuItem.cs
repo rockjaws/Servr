8d03b37d6b72766a5a2774c61dbc3221acb98d16
Oliver Mosgaard <dumbcaesar94@gmail.com>
2026-03-13 08:46:05 +0100

    MVVM

---
+++ Servr/Model/IMenuItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Servr.Model
+{
+    public interface IMenuItem
+    {
+    }
+}
---
+++ Servr/ViewModel/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Servr.ViewModel
+{
+    class MainViewModel
+    {
+    }
+}
