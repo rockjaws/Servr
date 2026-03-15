aa3dc8aa620a6310a72f9b10eae9880e0d559927
Oliver Mosgaard <dumbcaesar94@gmail.com>
2026-03-15 12:52:44 +0100

    decorator classes

---
+++ Servr/Domain/Model/MenuItemExtra.cs
@@ -0,0 +1,19 @@
+﻿using Servr.Domain.Interface;
+
+namespace Servr.Domain.Model;
+
+public class MenuItemExtra : MenuItemDecorator
+{
+    private readonly string _extraName;
+    private readonly decimal _extraPrice;
+
+    public MenuItemExtra(IMenuItem item, string extraName, decimal extraPrice)
+        : base(item)
+    {
+        _extraName = extraName;
+        _extraPrice = extraPrice;
+    }
+
+    public override string Name => $"{_menuItem.Name} + {_extraName}";
+    public override decimal Price => _menuItem.Price + _extraPrice;
+}
--- Servr/Domain/Model/Order.cs
+++ Servr/Domain/Model/Order.cs
@@ -8,8 +8,8 @@ public class Order : IOrder
     public int OrderId { get; }
     public int Table { get; }
     public OrderStatus Status { get; private set; }
-    public List<IDrink> Drinks { get; private set; }
-    public List<IMenuItem> Food { get; private set; }
+    public List<IDrink> Drinks { get; }
+    public List<IMenuItem> Food { get; }
 
     public Order(int orderId, int table, List<IDrink> drinks, List<IMenuItem> food)
     {
