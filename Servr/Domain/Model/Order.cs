0988f06e262f7e7b7989923c5cb1085a313c1ea2
Nicolaj Hansen <nicolaj_95@outlook.com>
2026-03-16 12:05:42 +0100

    Refactor main view model

--- Servr/Domain/Model/Order.cs
+++ Servr/Domain/Model/Order.cs
@@ -5,23 +5,23 @@ namespace Servr.Domain.Model;
 
 public class Order : IOrder
 {
-    public int OrderId { get; }
-    public int Table { get; }
-    public OrderStatus Status { get; private set; }
-    public List<IDrink> Drinks { get; }
-    public List<IMenuItem> Food { get; }
+  public int OrderId { get; }
+  public int Table { get; }
+  public OrderStatus Status { get; private set; }
+  public List<IDrink> Drinks { get; }
+  public List<IMenuItem> Food { get; }
 
-    public Order(int orderId, int table, List<IDrink> drinks, List<IMenuItem> food)
-    {
-        OrderId = orderId;
-        Status = OrderStatus.Received;
-        Table = table;
-        Drinks = drinks;
-        Food = food;
-    }
+  public Order(int orderId, int table, IEnumerable<IItem> items)
+  {
+    OrderId = orderId;
+    Status = OrderStatus.Received;
+    Table = table;
+    Drinks = items.OfType<IDrink>().ToList();
+    Food = items.OfType<IMenuItem>().ToList();
+  }
 
-    public void UpdateOrderStatus(OrderStatus status)
-    {
-        Status = status;
-    }
+  public void UpdateOrderStatus(OrderStatus status)
+  {
+    Status = status;
+  }
 }
