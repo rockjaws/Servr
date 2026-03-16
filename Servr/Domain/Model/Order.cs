16b50bfb47314094a2c464a78125acac16cdd3b7
Oliver Mosgaard <dumbcaesar94@gmail.com>
2026-03-16 13:10:31 +0100

    ability to set discount.

--- Servr/Domain/Model/Order.cs
+++ Servr/Domain/Model/Order.cs
@@ -5,23 +5,30 @@ namespace Servr.Domain.Model;
 
 public class Order : IOrder
 {
-  public int OrderId { get; }
-  public int Table { get; }
-  public OrderStatus Status { get; private set; }
-  public List<IDrink> Drinks { get; }
-  public List<IMenuItem> Food { get; }
+    public int OrderId { get; }
+    public int Table { get; }
+    public OrderStatus Status { get; private set; }
+    public DiscountType Discount { get; }
+    public List<IDrink> Drinks { get; }
+    public List<IMenuItem> Food { get; }
 
-  public Order(int orderId, int table, IEnumerable<IItem> items)
-  {
-    OrderId = orderId;
-    Status = OrderStatus.Received;
-    Table = table;
-    Drinks = items.OfType<IDrink>().ToList();
-    Food = items.OfType<IMenuItem>().ToList();
-  }
+    public Order(
+        int orderId,
+        int table,
+        IEnumerable<IItem> items,
+        DiscountType discount = DiscountType.None
+    )
+    {
+        OrderId = orderId;
+        Status = OrderStatus.Received;
+        Table = table;
+        Discount = discount;
+        Drinks = items.OfType<IDrink>().ToList();
+        Food = items.OfType<IMenuItem>().ToList();
+    }
 
-  public void UpdateOrderStatus(OrderStatus status)
-  {
-    Status = status;
-  }
+    public void UpdateOrderStatus(OrderStatus status)
+    {
+        Status = status;
+    }
 }
